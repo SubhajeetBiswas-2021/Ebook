b035d46cb369be51d2ad73767053b47e16edfa5e
SubhajeetBiswas-2021 <subhajeet.bs2021@gmail.com>
2025-07-09 13:02:17 +0530

    Commit

---
+++ app/src/main/java/com/subhajeet/ebook/di/HiltModule.kt
@@ -0,0 +1,18 @@
+package com.subhajeet.ebook.di
+
+import com.google.firebase.database.FirebaseDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object HiltModule {
+
+    @Provides
+    fun provideFirebaseDatabase():FirebaseDatabase{
+        return FirebaseDatabase.getInstance()
+    }
+}

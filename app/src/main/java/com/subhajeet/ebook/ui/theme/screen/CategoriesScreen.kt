c5d9af617b11bdae2c0a1b434279885613f916f2
SubhajeetBiswas-2021 <subhajeet.bs2021@gmail.com>
2025-07-17 16:36:05 +0530

    Commit

--- app/src/main/java/com/subhajeet/ebook/ui/theme/screen/CategoriesScreen.kt
+++ app/src/main/java/com/subhajeet/ebook/ui/theme/screen/CategoriesScreen.kt
@@ -14,7 +14,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Face
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier

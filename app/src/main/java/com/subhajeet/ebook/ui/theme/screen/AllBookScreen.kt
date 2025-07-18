901f67a9e9eca563a5599b4d89303a436fffd18f
SubhajeetBiswas-2021 <subhajeet.bs2021@gmail.com>
2025-07-18 23:40:40 +0530

    Commit

--- app/src/main/java/com/subhajeet/ebook/ui/theme/screen/AllBookScreen.kt
+++ app/src/main/java/com/subhajeet/ebook/ui/theme/screen/AllBookScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -90,4 +91,5 @@ fun eachCard(title: String,author:String,bookImage:String ) {
 
 
 }
+
 

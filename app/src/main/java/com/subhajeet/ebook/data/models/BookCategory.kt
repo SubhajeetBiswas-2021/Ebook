a30b195998cb17776fe47103be51b367194cc531
SubhajeetBiswas-2021 <subhajeet.bs2021@gmail.com>
2025-07-18 23:41:41 +0530

    Commit

--- app/src/main/java/com/subhajeet/ebook/data/models/BookCategory.kt
+++ app/src/main/java/com/subhajeet/ebook/data/models/BookCategory.kt
@@ -1,6 +1,6 @@
 package com.subhajeet.ebook.data.models
 
 data class BookCategory(
-    var categoryName:String,
-    var categoryImageUrl: String,
+    var categoryName:String="",
+    var categoryImageUrl: String="",
 )

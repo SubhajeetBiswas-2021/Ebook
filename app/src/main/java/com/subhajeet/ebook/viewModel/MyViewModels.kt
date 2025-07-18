3c54e076ba0d248ee015dd508a0d70bc4076aabb
SubhajeetBiswas-2021 <subhajeet.bs2021@gmail.com>
2025-07-18 23:41:12 +0530

    Commit

--- app/src/main/java/com/subhajeet/ebook/viewModel/MyViewModels.kt
+++ app/src/main/java/com/subhajeet/ebook/viewModel/MyViewModels.kt
@@ -3,6 +3,7 @@ package com.subhajeet.ebook.viewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.subhajeet.ebook.common.ResultState
+import com.subhajeet.ebook.data.models.BookCategory
 import com.subhajeet.ebook.data.models.bookModel
 import com.subhajeet.ebook.repo.Repo
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -18,9 +19,14 @@ class MyViewModels @Inject constructor(private val repo: Repo) : ViewModel() {
     private val _getAllBookState = MutableStateFlow(GetAllBookState())   //if this is used then we change the state from outside only so make it a private variable first and then access it via a public variable
     val getAllBookState = _getAllBookState.asStateFlow()   //it is the public variable and we will get the as soon we access this
 
+    private val _getAllCategoryState = MutableStateFlow(GetAllCategoryState())
+    val getAllCategoryState = _getAllCategoryState.asStateFlow()
+
     init {
         getAllBooks()
+        getAllCategories()
     }
+
     fun getAllBooks(){
         viewModelScope.launch(Dispatchers.IO) {
             repo.getAllBooks().collect{
@@ -47,10 +53,43 @@ class MyViewModels @Inject constructor(private val repo: Repo) : ViewModel() {
             }
         }
     }
+
+    fun getAllCategories(){
+        viewModelScope.launch(Dispatchers.IO) {
+            repo.getAllCategories().collect{
+                when(it){
+                    is ResultState.Loading ->{
+                        _getAllCategoryState.value= GetAllCategoryState(
+                            isLoading = true
+                        )
+                    }
+                    is ResultState.Error ->{ //if getting error passing to UI
+                        _getAllCategoryState.value= GetAllCategoryState(
+                            isLoading = false,
+                            error = it.message,
+                        )
+
+                    }
+                    is ResultState.Success ->{
+                        _getAllCategoryState.value= GetAllCategoryState(
+                            isLoading = false,
+                            success = it.data
+                        )
+                    }
+                }
+            }
+        }
+    }
 }
 
 data class GetAllBookState(
     val isLoading:Boolean=false,
     val success: List<bookModel> = emptyList(),
+    val error:String?=null
+)
+
+data class GetAllCategoryState(
+    val isLoading:Boolean=false,
+    val success: List<BookCategory> = emptyList(),
     val error:String?=null
 )

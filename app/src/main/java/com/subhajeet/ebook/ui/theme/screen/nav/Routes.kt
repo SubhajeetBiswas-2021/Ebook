4217d237f109c4897214299cd0950fc767f7385b
SubhajeetBiswas-2021 <subhajeet.bs2021@gmail.com>
2025-07-20 20:25:18 +0530

    Commit

---
+++ app/src/main/java/com/subhajeet/ebook/ui/theme/screen/nav/Routes.kt
@@ -0,0 +1,21 @@
+package com.subhajeet.ebook.ui.theme.screen.nav
+
+import kotlinx.serialization.Serializable
+
+sealed class Routes {
+
+    @Serializable
+    object TabScreen
+
+    @Serializable
+    object CategoriesScreen
+
+    @Serializable
+    object AllBookScreen
+
+    @Serializable
+    data class BookByCategory(
+        val categoryName:String?= null
+    )
+
+}

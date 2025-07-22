12941076fe79cea88d994661711628e0cd8e6ac7
SubhajeetBiswas-2021 <subhajeet.bs2021@gmail.com>
2025-07-22 14:48:23 +0530

    Commit

--- app/src/main/java/com/subhajeet/ebook/ui/theme/screen/BookByCategory.kt
+++ app/src/main/java/com/subhajeet/ebook/ui/theme/screen/BookByCategory.kt
@@ -12,13 +12,19 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -30,6 +36,7 @@ import androidx.navigation.NavController
 import coil3.compose.AsyncImage
 import com.subhajeet.ebook.viewModel.MyViewModels
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BookByCategory(modifier: Modifier = Modifier,categoryName: String,viewModels: MyViewModels = hiltViewModel(), navController: NavController) {
 
@@ -37,19 +44,42 @@ fun BookByCategory(modifier: Modifier = Modifier,categoryName: String,viewModels
     LaunchedEffect(categoryName) {
         viewModels.getBookByCategory(categoryName)
     }
+    var scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 
     val getBookByCategoryState = viewModels.getBookByCategoryState.collectAsState()
-    when{
-        getBookByCategoryState.value.isLoading ->{
-            CircularProgressIndicator()
-        }
-        getBookByCategoryState.value.error != null ->{
-            Text(text="Error: ${getBookByCategoryState.value.error}",modifier = modifier)
-        }
-        getBookByCategoryState.value.success.isNotEmpty() ->{
-            LazyColumn {
-                items(getBookByCategoryState.value.success){
-                    EachCard(title = it.bookName, author = it.bookAuthor, bookImage = it.bookImage)
+    Scaffold(modifier = Modifier.fillMaxSize().nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text(categoryName)
+                },
+                scrollBehavior = scrollBehavior
+            )
+
+        })
+    { innerPadding ->
+        when {
+            getBookByCategoryState.value.isLoading -> {
+                CircularProgressIndicator()
+            }
+
+            getBookByCategoryState.value.error != null -> {
+                Text(text = "Error: ${getBookByCategoryState.value.error}", modifier = modifier)
+            }
+
+            getBookByCategoryState.value.success.isNotEmpty() -> {
+                LazyColumn(modifier=Modifier.padding(0.dp,100.dp,0.dp,0.dp)) {
+                    items(getBookByCategoryState.value.success) {
+                        EachCard(
+                            title = it.bookName,
+                            author = it.bookAuthor,
+                            bookImage = it.bookImage
+                        )
+                    }
                 }
             }
         }
@@ -57,12 +87,11 @@ fun BookByCategory(modifier: Modifier = Modifier,categoryName: String,viewModels
 }
 
 
-
 //@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun EachCard(title: String, author: String, bookImage: String) {
     Card(modifier = Modifier
-        .padding(8.dp, 34.dp, 0.dp, 0.dp)
+        .padding(16.dp,28.dp,16.dp,0.dp)
         .height(140.dp)) {
 
 

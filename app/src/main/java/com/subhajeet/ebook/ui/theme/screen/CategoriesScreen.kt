999fbf16034fc0f5a258cad46c151b809382f8d3
SubhajeetBiswas-2021 <subhajeet.bs2021@gmail.com>
2025-08-26 12:48:36 +0530

    Commit

--- app/src/main/java/com/subhajeet/ebook/ui/theme/screen/CategoriesScreen.kt
+++ app/src/main/java/com/subhajeet/ebook/ui/theme/screen/CategoriesScreen.kt
@@ -1,23 +1,16 @@
 package com.subhajeet.ebook.ui.theme.screen
 
 import android.widget.Toast
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Face
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
@@ -31,13 +24,11 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import coil3.compose.AsyncImage
-import com.subhajeet.ebook.data.models.BookCategory
 import com.subhajeet.ebook.ui.theme.screen.nav.Routes
 import com.subhajeet.ebook.viewModel.MyViewModels
 
